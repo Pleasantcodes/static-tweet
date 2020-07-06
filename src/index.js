a18c885100cc23d76de6df72e5fa3472b7f33fd4
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-07-06 02:57:13 -0700

    building components based tweet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 import "./index.css";
 
-const Time = () => <span className="time">3h ago</span>;
+// const Time = () => <span className="time">3h ago</span>;
 const ReplyButton = () => <i className="fa fa-reply reply-button" />;
 const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
 const LikeButton = () => <i className="fa fa-heart like-button" />;
@@ -16,7 +17,8 @@ const MoreOptionsButton = () => (
 
 //real
 
-function Avatar() {
+function Avatar({ hash }) {
+  var url = `https://media-exp1.licdn.com/dms/image/C5603AQEYXkY6AzWy7w/profile-displayphoto-shrink_200_200/0?e=1599091200&v=beta&t=we7Y9KxqXcKyMa1oHtexzYqjcblNB3zQVxiPhD12NeA/${hash}`;
   return (
     <img
       src="https://media-exp1.licdn.com/dms/image/C5603AQEYXkY6AzWy7w/profile-displayphoto-shrink_200_200/0?e=1599091200&v=beta&t=we7Y9KxqXcKyMa1oHtexzYqjcblNB3zQVxiPhD12NeA"
@@ -26,37 +28,39 @@ function Avatar() {
   );
 }
 
-function Message() {
-  return (
-    <div className="message">
-      I'm a creative Front-end Developer with 3+ years of experience leveraging
-      mark-up, js language with CMS to build responsive websites, and
-      interactive features that drive business growth and improve UX.
-    </div>
-  );
+function Message({ text }) {
+  return <div className="message">{text}</div>;
 }
 
-function NameWithHandle() {
+function NameWithHandle({ author }) {
+  const { name, handle } = author;
   return (
     <span className="name-with-handle">
-      <span className="name">Gàjí Fún rẹ́ ẹ (Pleasant Codes)</span>
-      <span className="handle">@Pleasant_codes</span>
+      <span className="name">{name}</span>
+      <span className="handle">@{handle}</span>
     </span>
   );
 }
 
-function Tweet() {
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return <span className="time">{timeString}</span>;
+};
+
+// add the { tweet } prop, destructured
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <Avatar />
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <NameWithHandle />
-        <Time />
-        <Message />
+        <NameWithHandle author={tweet.author} />
+        <Time time={tweet.timestamp} />
+        <Message text={tweet.message} />
         <div className="buttons">
           <ReplyButton />
           <RetweetButton />
           <LikeButton />
+          61 Contents
           <MoreOptionsButton />
         </div>
       </div>
@@ -64,6 +68,24 @@ function Tweet() {
   );
 }
 
-ReactDOM.render(<Tweet />, document.querySelector("#root"));
+// ....
+
+var testTweet = {
+  message: "Something about cats.",
+  gravatar: "xyz",
+  author: {
+    handle: "catperson",
+    name: "IAMA Cat Person",
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: "2016-07-30 21:24:37",
+};
+
+ReactDOM.render(
+  <Tweet tweet={testTweet} />,
+
+  document.querySelector("#root")
+);
 
 // https://www.gravatar.com/avatar/nothing
